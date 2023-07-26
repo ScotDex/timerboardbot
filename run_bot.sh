ae55ebc57c8ed0b6e6e7308ae328dc37d60fef32
ScotDex <35346954+ScotDex@users.noreply.github.com>
2023-07-26 15:37:50 +0100

    Update run_bot.sh

--- run_bot.sh
+++ run_bot.sh
@@ -4,7 +4,7 @@
 docker build -t my_discord_bot .
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token from the Discord Developer Portal
-BOT_TOKEN="YOUR_BOT_TOKEN"
+BOT_TOKEN="MTEzMzc1NDc3NjQ2MzYyNjI4MA.GOB_gq.Y0kNW-W2XDmpznOwesFycVDLKRsWw1YSqFndSA"
 
 # Run the Docker container with environment variable for bot token
 docker run -e BOT_TOKEN="$BOT_TOKEN" my_discord_bot
